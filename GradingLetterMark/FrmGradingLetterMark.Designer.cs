482b1b8a83a59627692bb162d26f3a2f61c0757d
Irtaza Chohan <irtazachohan@gmail.com>
2020-04-19 17:27:22 +0100

    Add project files.

---
+++ GradingLetterMark/FrmGradingLetterMark.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GradingLetterMark
+{
+    partial class FrmGradingLetterMark
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.Location = new System.Drawing.Point(146, 97);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(140, 44);
+            this.BtnCalculate.TabIndex = 0;
+            this.BtnCalculate.Text = "Calculate";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // FrmGradingLetterMark
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 271);
+            this.Controls.Add(this.BtnCalculate);
+            this.Name = "FrmGradingLetterMark";
+            this.Text = "Grading Letter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCalculate;
+    }
+}
+
---
+++ GradingLetterMark/FrmGradingLetterMark.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradingLetterMark
+{
+    public partial class FrmGradingLetterMark : Form
+    {
+        public FrmGradingLetterMark()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            int grade;
+            string mark;
+
+            grade = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter in your mark", "Mark calculator", ""));
+
+            if (grade >= 0 && grade < 50)
+            {
+                mark = "F";
+            }
+            else if (grade >=50 && grade < 60)
+            {
+                mark = "D";
+            }
+            else if (grade >=60 && grade < 70)
+            {
+                mark = "C";
+            }
+            else if (grade >=70 && grade < 80)
+            {
+                mark = "B";
+            }
+            else if (grade >= 80 && grade < 100)
+            {
+                mark = "A";
+            }
+            else
+            {
+                mark = "Unable to calculate";
+            }
+
+            if (mark == "Unable to calculate")
+            {
+                MessageBox.Show("Cannot calculate grade");
+            }
+            else
+            {
+                MessageBox.Show("You have achieved a grade of " + mark);
+            }
+        }
+    }
+}
